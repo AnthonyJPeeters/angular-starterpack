f857fdb68b6f3948f378cf74fd24b5e45d46db67
Anthony <anthonypeeters221@hotmail.com>
2017-11-23 16:34:41 +0100

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'https://nosqltestproject.herokuapp.com/api/v1/'
+  serverUrl: 'https://nosqltestproject.herokuapp.com/api/v1//'
 };
